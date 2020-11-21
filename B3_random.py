7d757a58064b9b1c424a6d1d46450749564c3350
Analhy <anaochoav@gmail.com>
2020-11-21 14:31:41 -0600

    My first commit

---
+++ B3_random.py
@@ -0,0 +1,10 @@
+#working in random
+
+from random import randint
+
+elemento= int(input('Los elementos requeridos:'))
+
+contador=1
+while contador <= elemento:
+    print ('Contador')
+    contador =contador+1
