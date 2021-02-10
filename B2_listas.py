101a25c65e94eab71159e7f27fcae29d044c23b0
sumy2776 <72669172+sumy2776@users.noreply.github.com>
2021-02-10 14:31:28 -0600

    Update B2_listas.py

--- B2_listas.py
+++ B2_listas.py
@@ -9,3 +9,14 @@ print (numeros)
 
 numeros.pop()
 print (numeros)
+
+numeros.reverse()
+print (numeros)
+
+for x in numeros:
+  print ("you have the number %d of %d" % (x, len(numeros)))
+  print ("your number is at %d position" % numeros.index(x))
+  print ("your number appears %d times" % numeros.count(x))
+  
+numeros[4]=1
+print (numeros)
