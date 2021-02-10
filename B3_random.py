8c01417441f3808307cb4c849111fcdf21d75e0d
Analhy <anaochoav@gmail.com>
2021-02-10 14:43:38 -0600

    Practical cases

--- B3_random.py
+++ B3_random.py
@@ -3,8 +3,13 @@
 from random import randint
 
 elemento= int(input('Los elementos requeridos:'))
-
+lista_aleatoria=[]
 contador=1
 while contador <= elemento:
-    print ('Contador')
+
+    matriz = randint(1,100)
+    val=elemento*matriz
+
+    lista_aleatoria.append(val)
     contador =contador+1
+print (lista_aleatoria)
